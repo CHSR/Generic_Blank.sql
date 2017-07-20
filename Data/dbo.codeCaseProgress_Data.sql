73eb5f739e86ce2c618db0909b5caf073b58a932
Benjamin Simmons <brsimmons@albany.edu>
2017-07-20 13:23:13 -0400

    Linked static data

---
+++ Data/dbo.codeCaseProgress_Data.sql
@@ -0,0 +1,15 @@
+SET IDENTITY_INSERT [dbo].[codeCaseProgress] ON
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (1, 1.0, 'Screen', 'HVScreen', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (2, 2.0, 'PA-Stat=1', 'Preassessment-Casestatus=1', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (3, 3.0, 'PA-Stat=3', 'Preassessment-Casestatus=3	', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (4, 4.0, 'PA-Stat=2', 'Preassessment-Casestatus=2', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (5, 5.0, 'Kempe', 'Kempe', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (6, 6.0, 'PI-Stat=1', 'Preintake-Casestatus=1', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (7, 7.0, 'PI-Stat=3', 'Preintake-Casestatus=3', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (8, 8.0, 'PI-Stat=2', 'Preintake-Casestatus=2', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (9, 9.0, 'ID Contact', 'ID Contact', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (10, 10.0, 'Intake', 'Intake', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (11, 11.0, 'TC ID', 'TC ID', NULL)
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (12, 0.5, 'Scrn-term', 'HVScreen-terminated', 'referral not made')
+INSERT INTO [dbo].[codeCaseProgress] ([codeCaseProgressPK], [CaseProgressCode], [CaseProgressBrief], [CaseProgressDescription], [CaseProgressNote]) VALUES (14, 4.5, 'Kempe-term', 'Preassessment-CasesStatus=2, not assigned', NULL)
+SET IDENTITY_INSERT [dbo].[codeCaseProgress] OFF
